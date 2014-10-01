37ac104a3271c11cc9c6c2d844cbe8e5e968fefa
Crt Gregoric <crt.gregoric@gmail.com>
2014-10-01 23:59:19 +0200

    Added socket stuff + basic commands

--- robot.py
+++ robot.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 from libraries.Adafruit_PWM_Servo_Driver import PWM
+import socket
 
 from pwm_objects.tower_pro_sg5010 import TowerProSG5010
 from pwm_objects.tower_pro_sg90 import TowerProSG90
@@ -21,6 +22,9 @@ class Robot():
 
     LED1_CHANNEL = 6
 
+    HOST_NAME = 'rpi.local'
+    PORT_NUMBER = 1234
+
     def __init__(self):
         pwm = PWM(self.ADDRESS)
         pwm.setPWMFreq(self.FREQUENCY)
@@ -32,5 +36,45 @@ class Robot():
 
         self.led1 = Led(pwm, self.LED1_CHANNEL)
 
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.bind((self.HOST_NAME, self.PORT_NUMBER))
+        self.socket.listen(1)
+
+        print('Ready.')
+
+        (self.connection, self.client_address) = self.socket.accept()
+        print('Connected by: {}', self.client_address[0])
+
+    def main_loop(self):
+        while True:
+            data = self.connection.recv(1024)
+
+            if data:
+                command = str(data).split()
+                value = int(command[1])
+
+                if command[0] == '0':
+                    self.right_motor.set_speed(value)
+
+                elif command[0] == '1':
+                    self.left_motor.set_speed(value)
+
+                elif command[0] == '2':
+                    self.camera_motor.set_angle(value)
+
+                elif command[0] == '3':
+                    self.led1.set_brightness(value)
+
+                elif command[0] == '4':
+                    self.connection.close()
+                    self.socket.close()
+                    break
+
+            else:
+                self.connection.close()
+                self.socket.close()
+                break
+
 
 robot = Robot()
+robot.main_loop()
